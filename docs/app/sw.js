0cafe838a40fd9b77b3a052ed0481b9d7e23cac4
Magno Benhur Garcia <magnobenhgarcia@192.168.1.22>
2026-06-02 09:06:27 -0300

    Replace app PWA with web tuner

--- docs/app/sw.js
+++ docs/app/sw.js
@@ -1,12 +1,36 @@
-const CACHE_NAME = 'meu-primeiro-afinador-v1';
+const CACHE_NAME = 'meu-primeiro-afinador-pwa-v2';
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './app-icon.png',
+  './store-cover.png',
+];
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./'])));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key)))))
+      .then(() => self.clients.claim()),
+  );
 });
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
+  );
 });
