d34564025aca7674389253c6612e443390994701
Magno Benhur Garcia <magnobenhgarcia@192.168.1.22>
2026-06-02 08:51:13 -0300

    Add PWA test build

---
+++ docs/app/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'meu-primeiro-afinador-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
